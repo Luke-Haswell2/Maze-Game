79a824787b839b4a7de29345467eb47510053546
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2023-11-01 14:26:59 +0000

    Script fixes

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.VFX;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 namespace Enemy
 {
